efa478898a88639fddf58bb2597f1dd284242220
meetme2meat <meetme2meat@gmail.com>
2015-12-20 05:48:13 -0500

    nginx template

--- modules/nginx/manifests/nginx.pp
+++ modules/nginx/manifests/nginx.pp
@@ -1,5 +1,21 @@
 class nginx {
+  package { 'apache2.2-common': 
+    ensure: absent
+  }
+
   package { 'nginx':
-    ensure => installed
-   }
+    ensure: installed,
+    require: Package['apache2.2-common']
+  }
+
+  service { 'nginx':
+    ensure: running,
+    enable: true,
+    require: Package['nginx']
+  }
+
+  file { 'nginx-conf': 
+    content: template('nginx/nginx.conf'),
+    notify: Service['nginx']
+  }
 }
