f6c7fbb0334fb285ee96c1b4b4db1e60eda38e31
meetme2meat <meetme2meat@gmail.com>
2015-12-23 13:43:25 +0530

    check how plugin works

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,13 @@
+
 node puppet {
   include sudoers
   include users
+  include '::mysql::server'
+  include '::mysql::client'
+  include '::mysql-bindings'
 } 
+
+class { '::mysql::server':
+  root_password           => 'root123',
+  remove_default_accounts => true
+}
